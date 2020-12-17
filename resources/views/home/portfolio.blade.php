b5716f2263ace2d73c5ae0d8b0950a248e7d8c35
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 21:30:39 -0300

    Modularizacion home portfolio

---
+++ resources/views/home/portfolio.blade.php
@@ -0,0 +1,47 @@
+<div id="portfolio" class="section md-padding bg-grey">
+    <div class="container">
+        <div class="row">
+            <div class="section-header text-center">
+                <h2 class="title">Portafolio</h2>
+            </div>
+            <div class="col-md-4 col-xs-6 work">
+                <img class="img-responsive" src="/images/deliciasurbanas-portfolio.jpg" alt="">
+                <div class="overlay"></div>
+                <div class="work-content">
+                    <span>Diseño de marca</span>
+                    <h3>Delicias Urbanas</h3>
+                    <div class="work-link">
+                        <!--a href="#"><i class="fa fa-external-link"></i></a-->
+                        <a class="lightbox" href="/images/deliciasurbanas-portfolio.jpg"><i class="fa fa-search"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-6 work">
+                <img class="img-responsive" src="{{asset('/images/comerciocentral-portfolio.jpg')}}" alt="Comercio Central Website">
+                <div class="overlay"></div>
+                <div class="work-content">
+                    <span>E-commerce App</span>
+                    <h3>Comercio Central</h3>
+                    <div class="work-link">
+                        <a href="https://comerciocentral.cl"><i class="fa fa-link"></i></a>
+                        <a class="lightbox" href="{{asset('/images/comerciocentral-portfolio.jpg')}}"><i class="fa fa-search"></i></a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-4 col-xs-6 work">
+                <img class="img-responsive" src="{{asset('/images/estacionriquelme-portfolio.png')}}" alt="Comercio Central Website">
+                <div class="overlay"></div>
+                <div class="work-content">
+                    <span>Website</span>
+                    <h3>Estacion Riquelme</h3>
+                    <div class="work-link">
+                        <a href="https://estacionriquelme.cl"><i class="fa fa-link"></i></a>
+                        <a class="lightbox" href="{{asset('/images/estacionriquelme-portfolio.png')}}"><i class="fa fa-search"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
