f4d1439735e6d564be609c6aa971a364c204e767
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 21:27:07 -0300

    Modularizacion home about

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,33 @@
+<div id="about" class="section md-padding">
+    <div class="container">
+        <div class="row">
+            <div class="section-header text-center">
+                <h2 class="title">@lang('about.title')</h2>
+            </div>
+            <div class="col-md-4">
+                <div class="about">
+                    <i class="fa fa-cogs"></i>
+                    <h3>@lang('about.about_1_title')</h3>
+                    <p>@lang('about.about_1_content')</p>
+                    <a href="#">@lang('about.btn_more')</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="about">
+                    <i class="fab fa-aws"></i>
+                    <h3>@lang('about.about_2_title')</h3>
+                    <p>@lang('about.about_2_content')</p>
+                    <a href="#">@lang('about.btn_more')</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="about">
+                    <i class="fa fa-mobile"></i>
+                    <h3>@lang('about.about_3_title')</h3>
+                    <p>@lang('about.about_3_content')</p>
+                    <a href="#">@lang('about.btn_more')</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,40 +5,9 @@
 <!-- Header goes here -->
 @include('home.header')
 
+<!-- About goes here -->
+@include('home.about')
 
-<div id="about" class="section md-padding">
-    <div class="container">
-        <div class="row">
-            <div class="section-header text-center">
-                <h2 class="title">@lang('about.title')</h2>
-            </div>
-            <div class="col-md-4">
-                <div class="about">
-                    <i class="fa fa-cogs"></i>
-                    <h3>@lang('about.about_1_title')</h3>
-                    <p>@lang('about.about_1_content')</p>
-                    <a href="#">@lang('about.btn_more')</a>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="about">
-                    <i class="fab fa-aws"></i>
-                    <h3>@lang('about.about_2_title')</h3>
-                    <p>@lang('about.about_2_content')</p>
-                    <a href="#">@lang('about.btn_more')</a>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="about">
-                    <i class="fa fa-mobile"></i>
-                    <h3>@lang('about.about_3_title')</h3>
-                    <p>@lang('about.about_3_content')</p>
-                    <a href="#">@lang('about.btn_more')</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 <div id="portfolio" class="section md-padding bg-grey">
     <div class="container">
         <div class="row">
