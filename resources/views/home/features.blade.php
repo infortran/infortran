7a50d719e504318620741d91d45d9b76f827f55d
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 21:50:03 -0300

    Modularizacion home features

---
+++ resources/views/home/features.blade.php
@@ -0,0 +1,37 @@
+<div id="features" class="section md-padding bg-grey">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="section-header">
+                    <h2 class="title">@lang('services.why_title')</h2>
+                </div>
+                <p>@lang('services.why_subtitle')
+                </p>
+                <div class="feature">
+                    <i class="fa fa-check"></i>
+                    <p>@lang('services.why_item_1')</p>
+                </div>
+                <div class="feature">
+                    <i class="fa fa-check"></i>
+                    <p>@lang('services.why_item_2')</p>
+                </div>
+                <div class="feature">
+                    <i class="fa fa-check"></i>
+                    <p>@lang('services.why_item_3')</p>
+                </div>
+                <div class="feature">
+                    <i class="fa fa-check"></i>
+                    <p>@lang('services.why_item_4')</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div id="about-slider" class="owl-carousel owl-theme">
+                    <img class="img-responsive" src="/images/ssl.jpg" alt="">
+                    <img class="img-responsive" src="/images/about2.jpg" alt="">
+                    <img class="img-responsive" src="/images/about1.jpg" alt="">
+                    <img class="img-responsive" src="/images/about2.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
