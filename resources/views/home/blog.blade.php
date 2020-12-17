81368e01ac09105b397afbc14b3efeff2895111e
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 22:16:01 -0300

    Modularizacion home blog

---
+++ resources/views/home/blog.blade.php
@@ -0,0 +1,60 @@
+<div id="blog" class="section md-padding bg-grey">
+    <div class="container">
+        <div class="row">
+            <div class="section-header text-center">
+                <h2 class="title">@lang('news.title')</h2>
+            </div>
+            <div class="col-md-4">
+                <div class="blog">
+                    <div class="blog-img">
+                        <img class="img-responsive" src="/images/blog1.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <ul class="blog-meta">
+                            <li><i class="fa fa-user"></i>John doe</li>
+                            <li><i class="fa fa-clock-o"></i>18 Oct</li>
+                            <li><i class="fa fa-comments"></i>57</li>
+                        </ul>
+                        <h3>Molestie at elementum eu facilisis sed odio</h3>
+                        <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+                        <a href="blog-single.blade.php">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="blog">
+                    <div class="blog-img">
+                        <img class="img-responsive" src="/images/blog2.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <ul class="blog-meta">
+                            <li><i class="fa fa-user"></i>John doe</li>
+                            <li><i class="fa fa-clock-o"></i>18 Oct</li>
+                            <li><i class="fa fa-comments"></i>57</li>
+                        </ul>
+                        <h3>Molestie at elementum eu facilisis sed odio</h3>
+                        <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+                        <a href="blog-single">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="blog">
+                    <div class="blog-img">
+                        <img class="img-responsive" src="/images/blog3.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <ul class="blog-meta">
+                            <li><i class="fa fa-user"></i>John doe</li>
+                            <li><i class="fa fa-clock-o"></i>18 Oct</li>
+                            <li><i class="fa fa-comments"></i>57</li>
+                        </ul>
+                        <h3>Molestie at elementum eu facilisis sed odio</h3>
+                        <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+                        <a href="blog-single">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
