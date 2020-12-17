081f1fe01ec17a25a592757c1a72d9ee25fb21ff
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 22:10:51 -0300

    Modularizacion home team

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -20,75 +20,13 @@
 <!-- Numbers goes here -->
 @include('home.numbers')
 
+
 <!-- Pricing goes here -->
 
 
-<div id="team" class="section md-padding">
-    <div class="container">
-        <div class="section-header text-center">
-            <h2 class="title">@lang('team.title')</h2>
-        </div>
-        <div class="row" style="display: flex; align-items: center;justify-content: center">
+<!-- Team goes here -->
+@include('home.team')
 
-            <!--div-- class="col-sm-4">
-                <div class="team">
-                    <div class="team-img">
-                        <img class="img-responsive" src="/images/beto-team.jpg" alt="">
-                        <div class="overlay">
-                            <div class="team-social">
-                                <a href="#"><i class="fab fa-facebook"></i></a>
-                                <a href="#"><i class="fab fa-google-plus"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-content">
-                        <h3>Roberto Díaz</h3>
-                        <span>(CMO) Director de Marketing,</span>
-                        <span>Diseñador Web</span>
-                    </div>
-                </div>
-            </div-->
-            <div class="col-sm-4">
-                <div class="team">
-                    <div class="team-img">
-                        <img class="img-responsive" src="/images/freddy-team.jpg" alt="">
-                        <div class="overlay">
-                            <div class="team-social">
-                                <a href="#"><i class="fab fa-facebook"></i></a>
-                                <a href="#"><i class="fab fa-google-plus"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-content">
-                        <h3>Freddy Pérez</h3>
-                        <span>(CEO) Director General,</span>
-                        <span>Full Stack Developer</span>
-                    </div>
-                </div>
-            </div>
-            <!--div class="col-sm-4">
-                <div class="team">
-                    <div class="team-img">
-                        <img class="img-responsive" src="/images/team3.jpg" alt="">
-                        <div class="overlay">
-                            <div class="team-social">
-                                <a href="#"><i class="fab fa-facebook"></i></a>
-                                <a href="#"><i class="fab fa-google-plus"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-content">
-                        <h3>John Doe</h3>
-                        <span>Web Designer</span>
-                    </div>
-                </div>
-            </div-->
-        </div>
-    </div>
-</div>
 <div id="testimonial" class="section md-padding">
     <div class="bg-img" style="background-image: url('/images/background3.jpg');">
         <div class="overlay"></div>
