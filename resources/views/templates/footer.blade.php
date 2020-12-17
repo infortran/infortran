dd079fff1567b735a6107f417bab33c411405c64
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 22:21:11 -0300

    Modularizacion templates footer

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,31 @@
+<footer id="footer" class="sm-padding bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-logo">
+                    <a href="/"><img src="images/logo-250-alt.png" alt="logo"></a>
+                </div>
+                <ul class="footer-follow">
+                    <li><a href="https://www.facebook.com/Infortran.chile/"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="https://twitter.com/Infortran_CL"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href=""><i class="fab fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/freddy-p%C3%A9rez-pacheco"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                </ul>
+                <div class="footer-copyright">
+                    <p>Diseñado por <a href="https://infortran.cl" target="_blank">Infortran S.I.</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div id="back-to-top"></div>
+<!--div id="preloader">
+    <div class="preloader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</div-->
