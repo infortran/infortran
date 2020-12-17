83ee8897e875438851afaf83657ed5873e3046cd
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 21:44:28 -0300

    Modularizacion home service

---
+++ resources/views/home/service.blade.php
@@ -0,0 +1,51 @@
+<div id="service" class="section md-padding">
+    <div class="container">
+        <div class="row">
+            <div class="section-header text-center">
+                <h2 class="title">@lang('services.title')</h2>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="service">
+                    <i class="fa fa-gem"></i>
+                    <h3>@lang('services.serv_1_title')</h3>
+                    <p>@lang('services.serv_1_content')</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="service">
+                    <i class="fa fa-rocket"></i>
+                    <h3>@lang('services.serv_2_title')</h3>
+                    <p>@lang('services.serv_2_content')</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="service">
+                    <i class="fa fa-cogs"></i>
+                    <h3>@lang('services.serv_3_title')</h3>
+                    <p>@lang('services.serv_3_content')</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="service">
+                    <i class="fa fa-bullhorn"></i>
+                    <h3>@lang('services.serv_4_title')</h3>
+                    <p>@lang('services.serv_4_content')</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="service">
+                    <i class="fa fa-headset"></i>
+                    <h3>@lang('services.serv_5_title')</h3>
+                    <p>@lang('services.serv_5_content')</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="service">
+                    <i class="fa fa-flask"></i>
+                    <h3>@lang('services.serv_6_title')</h3>
+                    <p>@lang('services.serv_6_content')</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
