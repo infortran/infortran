4cd95f0d2ffca4ffb7f438cfb1269fb39f869a64
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 22:13:45 -0300

    Modularizacion home testimonial

---
+++ resources/views/home/testimonial.blade.php
@@ -0,0 +1,38 @@
+<div id="testimonial" class="section md-padding">
+    <div class="bg-img" style="background-image: url('/images/background3.jpg');">
+        <div class="overlay"></div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div id="testimonial-slider" class="owl-carousel owl-theme">
+
+                    <div class="testimonial">
+                        <div class="testimonial-meta">
+                            <img src="/images/freddy-thumb.jpg" alt="">
+                            <h3 class="white-text">Freddy Pérez</h3>
+                            <span>Full Stack Developer</span>
+                        </div>
+                        <p class="white-text">
+                            "There have to be reasons that you get up in the morning and you want to live.
+                            Why do you want to live? What's the point? What inspires you? What do you love about the future?
+                            If the future does not include being out there among the stars and being a multi-planet species, I find that incredibly depressing."
+                            <br><em>Elon Musk.</em>
+                        </p>
+                    </div>
+
+                    <div class="testimonial">
+                        <div class="testimonial-meta">
+                            <!--img src="/images/beto-thumb.jpg" alt=""-->
+                            <h3 class="white-text">Coming soon</h3>
+                            <span>---</span>
+                        </div>
+                        <p class="white-text">
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
