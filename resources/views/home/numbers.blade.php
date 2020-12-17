087934201971c818d6c2784dd94bb832e9d2a758
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-16 22:07:51 -0300

    Modularizacion home pricing

---
+++ resources/views/home/numbers.blade.php
@@ -0,0 +1,37 @@
+<div id="numbers" class="section sm-padding">
+    <div class="bg-img" style="background-image: url('/images/background2.jpg');">
+        <div class="overlay"></div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 col-xs-6">
+                <div class="number">
+                    <i class="fa fa-users"></i>
+                    <h3 class="white-text"><span class="counter">soon</span></h3>
+                    <span class="white-text">Clientes</span>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="number">
+                    <i class="fa fa-trophy"></i>
+                    <h3 class="white-text"><span class="counter">soon</span></h3>
+                    <span class="white-text">Reconocimientos</span>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="number">
+                    <i class="fa fa-coffee"></i>
+                    <h3 class="white-text"><span class="counter">154</span>K</h3>
+                    <span class="white-text">Tazas de café</span>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="number">
+                    <i class="fa fa-file"></i>
+                    <h3 class="white-text"><span class="counter">1</span></h3>
+                    <span class="white-text">Projectos completados</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/home/pricing.blade.php
@@ -0,0 +1,81 @@
+<div id="pricing" class="section md-padding">
+    <div class="container">
+        <div class="row">
+            <div class="section-header text-center">
+                <h2 class="title">Precios</h2>
+            </div>
+            <div class="col-sm-4">
+                <div class="pricing">
+                    <div class="price-head">
+                        <span class="price-title">Basic plan</span>
+                        <div class="price">
+                            <h3>$9<span class="duration">/ month</span></h3>
+                        </div>
+                    </div>
+                    <ul class="price-content">
+                        <li>
+                            <p>1GB Disk Space</p>
+                        </li>
+                        <li>
+                            <p>100 Email Account</p>
+                        </li>
+                        <li>
+                            <p>24/24 Support</p>
+                        </li>
+                    </ul>
+                    <div class="price-btn">
+                        <button class="outline-btn">Purchase now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="pricing">
+                    <div class="price-head">
+                        <span class="price-title">Silver plan</span>
+                        <div class="price">
+                            <h3>$19<span class="duration">/ month</span></h3>
+                        </div>
+                    </div>
+                    <ul class="price-content">
+                        <li>
+                            <p>1GB Disk Space</p>
+                        </li>
+                        <li>
+                            <p>100 Email Account</p>
+                        </li>
+                        <li>
+                            <p>24/24 Support</p>
+                        </li>
+                    </ul>
+                    <div class="price-btn">
+                        <button class="outline-btn">Purchase now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="pricing">
+                    <div class="price-head">
+                        <span class="price-title">Gold plan</span>
+                        <div class="price">
+                            <h3>$39<span class="duration">/ month</span></h3>
+                        </div>
+                    </div>
+                    <ul class="price-content">
+                        <li>
+                            <p>1GB Disk Space</p>
+                        </li>
+                        <li>
+                            <p>100 Email Account</p>
+                        </li>
+                        <li>
+                            <p>24/24 Support</p>
+                        </li>
+                    </ul>
+                    <div class="price-btn">
+                        <button class="outline-btn">Purchase now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
