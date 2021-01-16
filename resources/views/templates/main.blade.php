6aef4356d0be9787cc74bd3f1863a5e56e238549
Freddy Perez <freddy.perez.trabajos@gmail.com>
2021-01-16 14:43:54 -0300

    Ajuste javascript defer

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <script src="{{asset('js/gtm.js')}}"></script>
+    <script src="{{asset('js/gtm.js')}}" defer></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,7 +23,7 @@
     <link type="text/css" rel="stylesheet" href="{{asset('css/magnific-popup.css')}}" />
     <link rel="stylesheet" href="{{asset('css/fontawesome.css')}}">
     <link type="text/css" rel="stylesheet" href="{{asset('css/style.css')}}" />
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="https://www.google.com/recaptcha/api.js" defer></script>
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
