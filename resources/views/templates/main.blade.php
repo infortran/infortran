e7bc0b6b26fb841fe8e4016c7a21f67f7ad59d58
Freddy Perez <freddy.perez.trabajos@gmail.com>
2021-01-16 14:55:06 -0300

    Correcion de title 103 a 60 caracteres

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Infortran | Servicios informáticos, Creación de sitios web, Mantencion de sitios, y tiendas e-commerce.</title>
+    <title>Infortran | Servicios informáticos, Chile.</title>
     <meta name="description" content="Servicios Informáticos de confianza, Creación de sitios web, Mantencion de sitios y tiendas e-commerce, VTEX, Magento, Creación de Apps Multiplataforma, Diseño de UX, Programación Laravel PHP." />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="author" content="Infortran, Servicios Informáticos - Freddy Pérez" />
